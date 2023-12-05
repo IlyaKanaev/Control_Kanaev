0d93a8e32ab752877979d17d7b64cd440aef3c70
Prios <kanaev.i.v@yandex.ru>
2023-12-05 14:54:26 +0300

    nitial commit

---
+++ Task/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] array = { "Hello", "2", "world", ":-)" };
+
+int result_count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        result_count++;
+    }
+}
+
+string[] result = new string[result_count];
+
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        result[j] = array[i];
+        j++;
+    }
+}
+
+for (int i = 0; i < result_count; i++)
+{
+    Console.WriteLine(result[i]);
+}
